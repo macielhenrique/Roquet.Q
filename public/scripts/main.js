0d6ef7e5e1ed9f663da7853c488586fd90e21109
macielhenrique <jhenriquefmaciel@gmail.com>
2022-11-06 19:36:18 -0300

    concluindo modal e iniciando backend

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,15 +2,16 @@ import Modal from './modal.js'
 
 const modal=Modal()
 
+const modalTiltle = document.querySelector('.modal h2')
+const modalDescription = document.querySelector('.modal p')
+const modalButton = document.querySelector('.modal button')
+
 //pegar todos os botões que existe com classe check
 const checkButtons = document.querySelectorAll(".actions a.check")
 
 checkButtons.forEach(button =>{
     //adicionar a escuta
-    button.addEventListener("click", event =>{
-      //abrir modal
-      modal.open()
-    }) 
+    button.addEventListener("click",handleClick) 
 })
 
 
@@ -18,7 +19,24 @@ checkButtons.forEach(button =>{
 const deleteButton = document.querySelectorAll(".actions a.delete")
 
 deleteButton.forEach(button =>{
-  button.addEventListener("click", event =>{
-    modal.open()
-  })
+  button.addEventListener("click",(event)=> handleClick(event, false))
 })
+
+function handleClick(event, check = true){
+  event.preventDefault()
+  const text = check ? "Marca como lida" : "Excluir"
+  const slug = check ? "check" : "delete"
+  const roomId = document.querySelector("#room-id").dataset.id
+  const questionId= event.target.dataset.id
+
+  const form = document.querySelector(".modal form")
+  form.setAttribute("action",`/room/${roomId}/${questionId}/${slug}`)
+  
+  modalTiltle.innerHTML= `${text} esta pergunta`
+  modalDescription.innerHTML= `Tem certeza que deseja ${text.toLowerCase()} esta pergunta`
+  modalButton.innerHTML= `sim,${text.toLowerCase()}`
+  check ? modalButton.classList.remove("red") : modalButton.classList.add("red")
+  
+  //abrir modal
+  modal.open()
+}
