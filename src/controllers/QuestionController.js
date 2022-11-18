54e11a8139d922659a7a225c50a7422d1b5c038d
macielhenrique <jhenriquefmaciel@gmail.com>
2022-11-17 21:51:22 -0300

    defenindo as ações das questões

--- src/controllers/QuestionController.js
+++ src/controllers/QuestionController.js
@@ -1,13 +1,30 @@
 const Database= require('../db/config')
 
 module.exports ={
-  index(req,res){
-    const roomid = req.params.room
+  async index(req,res){
+    const db= await Database()
+    const roomId = req.params.room
     const questionId = req.params.question
     const action = req.params.action
     const password = req.body.password
+    
+    /*verificar se a senha está correta */
 
-    console.log(`room = ${roomid}, questionId= ${questionId}, action ${action} password ${password}`)
+    const verifyRoom = await db.get(`SELECT * FROM rooms WHERE id= ${roomId}`)
+    if(verifyRoom.pass==password){
+        if(action == "delete"){
+
+          await db.run(`DELETE FROM questions WHERE id= ${questionId}`)
+
+        }else if(action == "check"){
+          await db.run(`UPDATE questions SET read = 1 WHERE id = ${questionId}`)
+        }
+        res.redirect(`/room/${roomId}`)
+    }else{
+      res.render(`passincorret`,{roomId:roomId})
+    }
+
+
   },
 
   async create(req, res){
