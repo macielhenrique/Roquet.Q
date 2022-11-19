4dbb6ed278d8047601655dcb096f65f675103cf3
macielhenrique <jhenriquefmaciel@gmail.com>
2022-11-19 16:50:19 -0300

    conlusão do projeto,todas as funcionalidades ok

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -42,8 +42,22 @@ module.exports={
     const roomId = req.params.room
     const questions = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 0`)
     const questionsRead = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 1`)
-    
-    res.render("room", {roomId: roomId, questions: questions, questionsRead:questionsRead})
+    let isNoQuestions
 
+        if(questions.length ==0){
+            if(questionsRead.length == 0){
+                isNoQuestions = true
+            }
+        }
+
+        res.render("room", {roomId: roomId, questions: questions, questionsRead: questionsRead, isNoQuestions: isNoQuestions})
+
+  },
+
+  enter(req,res){
+    const roomId = req.body.roomId
+
+    res.redirect(`/room/${roomId}`)
   }
+
 }
